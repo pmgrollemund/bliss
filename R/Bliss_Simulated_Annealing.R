770aa501e4ef3fea6eb64399c61918951a9049af
sanchezi <isabelle.sanchez@inra.fr>
2018-09-07 15:53:32 +0200

    Check ex. SA

--- R/Bliss_Simulated_Annealing.R
+++ R/Bliss_Simulated_Annealing.R
@@ -2,7 +2,7 @@
 #' Bliss_Simulated_Annealing
 ################################# ----
 #' @description A Simulated Annealing algorithm to determine the Bliss estimate
-#'              (for only functional covariate), i.e. the minimum of the 
+#'              (for only functional covariate), i.e. the minimum of the
 #'              posterior expectation of the Bliss loss.
 #' @return a list containing:
 #' \describe{
@@ -41,11 +41,17 @@
 #' @examples
 #' data(data1)
 #' data(param1)
-#' #res_Simulated_Annealing <- Bliss_Simulated_Annealing(beta_functions,param1)
-#' #ylim <- range(c(res_Simulated_Annealing$Bliss_estimate,
-#'  #                res_Simulated_Annealing$posterior_expe))
-#' #plot(param$grid,res_Simulated_Annealing$Bliss_estimate,type="l",ylim=ylim)
-#' #lines(param$grid,res_Simulated_Annealing$posterior_expe,lty=2)
+#' param1$grids<-data1$grids
+#' # result of res_bliss1<-fit_Bliss(data=data1,param=param1)
+#' data(res_bliss1)
+#' beta_sample <- compute_beta_sample(posterior_sample=res_bliss1$posterior_sample,
+#'                                    param=param1,Q=2)
+#' param_test<-list(grid=param1$grids[[1]],iter=1e4,K=2)
+#' test<-Bliss_Simulated_Annealing(beta_sample[[1]],
+#'                  res_bliss1$posterior_sample$param$normalization_values[[1]],
+#'                  param=param_test)
+#' plot(param_test$grid,test$Bliss_estimate,type="l")
+#' lines(param_test$grid,test$Smooth_estimate,lty=2)
 Bliss_Simulated_Annealing <- function(beta_sample,normalization_values,param,progress=FALSE){
  # load optional objects
  grid <- param[["grid"]]
@@ -59,31 +65,31 @@ Bliss_Simulated_Annealing <- function(beta_sample,normalization_values,param,pro
  l_max     <- param[["l_max"]] # pas bon : changement de nom
  basis     <- param[["basis"]]
  p    <- length(grid)
- 
+
  # Initialize the necessary unspecified objects
  if(is.null(Temp_init)) Temp_init <- 1000
- if(is.null(k_max))     k_max     <- K  # PMG 22/06/18 
+ if(is.null(k_max))     k_max     <- K  # PMG 22/06/18
  if(is.null(iter_sann)) iter_sann <- 5e4
  if(is.null(times_sann))times_sann<- 100
- if(is.null(burnin))    burnin    <- floor(iter/5) 
+ if(is.null(burnin))    burnin    <- floor(iter/5)
  if(is.null(l_max))     l_max     <- floor(p/5) # XXX a changer ?
  if(is.null(basis))     basis     <- "Uniform"
- 
+
  # Check if the burnin isn't too large.
  if(2*burnin > iter){
   burnin <- floor(iter/5)
-  if(progress) 
+  if(progress)
    cat("\t Burnin is too large. New burnin : ",burnin,".\n")
  }
- 
+
  # dm and dl are related to the random walk.
  dm <- floor(p/5)+1
  dl <- floor(l_max/2)+1
- 
+
  # Compute the starting point
  starting_point = compute_starting_point(apply(beta_sample,2,mean))
  if(k_max < nrow(starting_point)) k_max <- nrow(starting_point)  # PMG 04/08/18
-  
+
  # Compute the Simulated Annealing algorithm (3 times to find a suitable value
  # for the initial temperature)
  res_sann <- Bliss_Simulated_Annealing_cpp(iter_sann,beta_sample,
@@ -93,7 +99,7 @@ Bliss_Simulated_Annealing <- function(beta_sample,normalization_values,param,pro
                                            progress,starting_point)
  min_loss <- min(res_sann$trace[,3*k_max+4])
  for(times in 1:times_sann ){
-  if(progress) 
+  if(progress)
    cat("\t ",times,".\n")
   res_sann_tmp <- Bliss_Simulated_Annealing_cpp(iter_sann,beta_sample,
                                                 grid,burnin,Temp_init,k_max,
@@ -106,24 +112,24 @@ Bliss_Simulated_Annealing <- function(beta_sample,normalization_values,param,pro
    min_loss <- min_loss_tmp
   }
  }
- 
+
  # Determine the estimate
  index <- which(res_sann$trace[,ncol(res_sann$trace)] %in%
                  min(res_sann$trace[,ncol(res_sann$trace)]))[1]
- 
+
  argmin <- res_sann$trace[index,]
  res_k  <- argmin[length(argmin)-1] # XXXXX selection avec nom "K"
- 
+
  # Compute the estimate
  b        <- argmin[1:res_k]
  m        <- argmin[1:res_k+  k_max]
  l        <- argmin[1:res_k+2*k_max]
  k        <- argmin[3*k_max+3]
  estimate <- compute_beta_cpp(b,m,l,grid,p,k,basis,normalization_values)
- 
+
  difference  = abs(estimate-res_sann$posterior_expe)
  sdifference = moving_average_cpp(difference,floor(p/10))
- 
+
  trace_names <- NULL
  for(k in 1:k_max){
   trace_names <- c(trace_names,paste("b",k,sep="_"))
@@ -135,7 +141,7 @@ Bliss_Simulated_Annealing <- function(beta_sample,normalization_values,param,pro
   trace_names <- c(trace_names,paste("l",k,sep="_"))
  }
  colnames(res_sann$trace) <- c(trace_names ,"accepted","choice","k","loss")
- 
+
  return(list(Bliss_estimate  = estimate,
              Smooth_estimate = res_sann$posterior_expe,
              trace           = res_sann$trace,
