0058aa9d2f1e29e59af64491916e21a189f6ab93
sanchezi <isabelle.sanchez@inrae.fr>
2024-02-28 11:04:02 +0100

    CRAN request: change @docType to "_PACKAGE_" in bliss.R

--- R/bliss.R
+++ R/bliss.R
@@ -4,10 +4,10 @@
 #' including two estimators of the coefficient function and an estimator of its support.
 #' A representation of the posterior distribution is also available.
 #'
-#' @docType package
 #' @name bliss
 #' @import Rcpp RcppArmadillo
 #' @importFrom Rcpp evalCpp cppFunction Rcpp.plugin.maker
 #' @useDynLib bliss, .registration = TRUE
 # @exportPattern "^[[:alpha:]]+"
+"_PACKAGE"
 NULL
